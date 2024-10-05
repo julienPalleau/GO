5f9c1d9765aaa245e724b6864fe0440fe96e9c83
julienPalleau <jpalleau@gmail.com>
2024-10-05 18:16:41 +0200

    C1-Billing System

--- BootDev/Ch3 Functions/C1-Billing System.go
+++ BootDev/Ch3 Functions/C1-Billing System.go
@@ -24,6 +24,35 @@ This function should take the number of messages sent, and return the relevant d
     10% for more than 1000 messages.
     20% for more than 5000 messages.
     0% for anything less.
-
-
 */
+
+package main
+
+import "fmt"
+
+func calculateFinalBill(costPerMessage float64, numMessage int) float64 {
+    finalBill := calculateBaseBill(costPerMessage, numMessage) - (calculateBaseBill(costPerMessage, numMessage)) * calculateDiscountRate(numMessage)
+    return finalBill
+}
+
+func calculateDiscountRate(messageSent int) float64 {
+    if messageSent > 1000 && messageSent <= 5000 {
+        return 0.1
+    }
+    if messageSent > 5000 {
+        return 0.2
+    }
+    return 0
+
+}
+
+// don't touch below this line
+
+func calculateBaseBill(costPerMessage float64, messageSent int) float64 {
+    return costPerMessage * float64(messageSent)
+}
+
+func main() {
+    fmt.Print(calculateFinalBill(10, 20000))
+
+}
---
+++ BootDev/Ch3 Functions/C2-Processing Orders.go
@@ -0,0 +1,19 @@
+// https://www.boot.dev/lessons/09628165-d910-4344-b2bf-c9145d6e6317
+
+/*
+Processing Orders
+
+Management thinks our branding is so creative that our SaaS customers will pay for Textio merch.
+Assignment
+
+Complete the placeOrder function.
+
+It returns a bool indicating whether the order was successful (true is a success) and a float64 representing the user's balance after the order. The placeOrder function should always return the account balance regardless of if it was adjusted or not.
+
+The amountInStock and calcPrice functions can be used to look up the current stock and price of an item.
+
+    If the quantity is greater than the amount in stock, the order should be rejected.
+    If the user doesn't have enough money in their account, the order should be rejected.
+    Otherwise, the order should be accepted and you should return the new balance.
+*/
+
